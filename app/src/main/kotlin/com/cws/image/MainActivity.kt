708792d25842d5e3eaedaeb0f1b89cca9752c007
Cort Spellman <spellman.cort@gmail.com>
2016-08-05 22:02:15 -0500

    Add navigation through Jedux store.

--- app/src/main/kotlin/com/cws/image/MainActivity.kt
+++ app/src/main/kotlin/com/cws/image/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.cws.image
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
@@ -15,21 +16,33 @@ import trikita.anvil.RenderableView
 import trikita.anvil.appcompat.v7.AppCompatv7DSL
 import trikita.anvil.appcompat.v7.AppCompatv7DSL.*
 import trikita.jedux.Action
+import trikita.jedux.Store
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(MainView(this))
+
+    val store = (application as App).store
+    store.dispatch(showCurrentView(this))
+  }
+
+  override fun onBackPressed() {
+    val store = (application as App).store
+    store.dispatch(navigateBack(this))
   }
 }
 
 data class LanguagesProps(val languages: ImmutableList<String>)
 
 class LanguagesView : RenderableView {
-  var c: Context? = null
+  var c: Context
   var props: LanguagesProps = LanguagesProps(languages = immutableListOf())
+  var store: Store<Action<Actions, *>, State>
 
-  constructor(c: Context) : super(c) { this.c = c }
+  constructor(c: Context) : super(c) {
+    this.c = c
+    this.store = (c.applicationContext as App).store
+  }
   constructor(c: Context, props: LanguagesProps) : this(c) { this.props = props }
 
   override fun view() {
@@ -59,7 +72,7 @@ class LanguagesView : RenderableView {
               ellipsize(TextUtils.TruncateAt.END)
               text(l)
             }
-            onClick { v -> store.dispatch(Action(Actions.SET_LANGUAGE, l)) }
+            onClick { v -> store.dispatch(setLanguage(l)) }
           }
         }
       }
@@ -70,9 +83,14 @@ class LanguagesView : RenderableView {
 data class InstructionsProps(val instructions: ImmutableList<Instruction>)
 
 class InstructionsView : RenderableView {
-  var props: InstructionsProps = InstructionsProps(instructions = immutableListOf())
+  var c: Context
+  var props: InstructionsProps = InstructionsProps(immutableListOf())
+  var store: Store<Action<Actions, *>, State>
 
-  constructor(c: Context) : super(c)
+  constructor(c: Context) : super(c) {
+    this.c = c
+    this.store = (c.applicationContext as App).store
+  }
   constructor(c: Context, props: InstructionsProps) : this(c) { this.props = props }
 
   override fun view() {
@@ -89,6 +107,11 @@ class InstructionsView : RenderableView {
             text(i.subject + " - " + i.language)
             margin(dip(0), dip(16))
             textColor(android.graphics.Color.BLACK)
+            onClick { v ->
+              store.dispatch(navigateTo(NavigationFrame("instruction",
+                                                        InstructionProps(i)),
+                                        c as Activity))
+            }
           }
         }
       }
@@ -96,16 +119,49 @@ class InstructionsView : RenderableView {
   }
 }
 
+data class VisibleInstructionsProps(val instructions: ImmutableList<Instruction>, val language: String)
+
+class VisibleInstructionsView : RenderableView {
+  var c: Context
+  var props: VisibleInstructionsProps = VisibleInstructionsProps(immutableListOf(), "")
+
+  constructor(c: Context) : super(c) { this.c = c }
+  constructor(c: Context, props: VisibleInstructionsProps) : this(c) { this.props = props }
+
+  override fun view() {
+    InstructionsView(c,
+                     InstructionsProps(getVisibleInstructions(props.instructions,
+                                                              props.language))).view()
+  }
+}
+
 class MainView(val c: Context) : RenderableView(c) {
+  val store: Store<Action<Actions, *>, State> = (c.applicationContext as App).store
+
   override fun view() {
     linearLayoutCompat {
       size(FILL, FILL)
       AppCompatv7DSL.orientation(LinearLayoutCompat.VERTICAL)
-      LanguagesView(c, LanguagesProps(languages = languages)).view()
-      InstructionsView(c,
-                       InstructionsProps(instructions = getVisibleInstructions(getInstructions(store.state),
-                                                                               store.state.language))
-      ).view()
+      LanguagesView(c, LanguagesProps(store.state.languages)).view()
+      VisibleInstructionsView(c,
+                              VisibleInstructionsProps(getInstructions(store.state),
+                                                       store.state.language)).view()
+    }
+  }
+}
+
+data class InstructionProps(val instruction: Instruction)
+
+class InstructionView : RenderableView {
+  var props: InstructionProps = InstructionProps(instruction = Instruction("", "", ""))
+
+  constructor(c: Context) : super(c)
+  constructor(c: Context, props: InstructionProps) : this(c) { this.props = props }
+
+  override fun view() {
+    appCompatTextView {
+      text(props.instruction.toString())
+      textColor(android.graphics.Color.BLACK)
     }
   }
 }
