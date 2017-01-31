9c9a9ceacfd8ad472776d1a704b5dc80a11bea2a
Cort Spellman <spellman.cort@gmail.com>
2017-01-31 00:42:49 -0600

    Rename data-binding var in RecyclerView view holder.

--- app/src/main/kotlin/com/cws/image/RecyclerViewDataBindingViewHolder.kt
+++ app/src/main/kotlin/com/cws/image/RecyclerViewDataBindingViewHolder.kt
@@ -4,8 +4,8 @@ import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
 
 class RecyclerViewDataBindingViewHolder(val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
-  fun bind(viewModel: Any) {
-    binding.setVariable(BR.viewModel, viewModel)
+  fun bind(item: Any) {
+    binding.setVariable(BR.item, item)
     binding.executePendingBindings()
   }
 }
