d9d102727527091d8314b3d210e6e2bd56f420e0
Cort Spellman <spellman.cort@gmail.com>
2017-01-31 00:34:45 -0600

    Rename InstructionActivity to PlayInstructionActivity.

--- app/src/main/kotlin/com/cws/image/PlayInstructionActivity.kt
+++ app/src/main/kotlin/com/cws/image/PlayInstructionActivity.kt
@@ -3,7 +3,7 @@ package com.cws.image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class InstructionActivity : AppCompatActivity() {
+class PlayInstructionActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.instruction_activity)
