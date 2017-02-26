spellman/Image

commit 4ceb8782d781f10f1aceca05eb07a1b2edaa8895

Replace Log statements w/Timber. Clean up logging.
I enabled DebugTree and CrashlyticsTree loggers in debug build,
CrashlyticsTree in release.

I removed many Log.d statements that had accumulated.

app/src/main/kotlin/com/cws/image/MainActivity.kt
@@ -14,7 +14,6 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.View
 import android.widget.TextView
 import com.cws.image.databinding.MainActivityBinding
@@ -24,6 +23,7 @@ import com.jakewharton.rxbinding.support.design.widget.RxTabLayout
 import hu.akarnokd.rxjava.interop.RxJavaInterop
 import io.reactivex.disposables.Disposable
 import paperparcel.PaperParcel
+import timber.log.Timber
 
 // 2017-02-19 Cort Spellman
 // TODO: Put version info in a menu item that doesn't do anything when clicked.
@@ -81,18 +81,13 @@ class MainActivity : AppCompatActivity() {
       RxJavaInterop.toV2Observable(
         RxTabLayout.selections(binding.languages))
         .map { tab -> tab.tag as String }
-        .doOnNext { language ->
-          Log.d(this.javaClass.simpleName, "RxTabLayout.selections")
-          Log.d("selected tab", language)
-        }
         .subscribe { language ->
+          Timber.i("Selected language: ${language}")
           presenter.showInstructionsForLanguage(language)
         }
 
     if (savedInstanceState != null) {
-      Log.d(this.javaClass.simpleName, "restoring savedInstanceState")
       presenter.selectedLanguage = savedInstanceState.getString(SELECTED_LANGUAGE)
-      Log.d(this.javaClass.simpleName, "selectedLanguage: ${presenter.selectedLanguage}")
     }
 
     showInstructions()
@@ -100,8 +95,6 @@ class MainActivity : AppCompatActivity() {
 
   override fun onSaveInstanceState(outState: Bundle?) {
     super.onSaveInstanceState(outState)
-    Log.d(this.javaClass.simpleName,
-          "onSaveInstanceState selectedLanguage: ${presenter.selectedLanguage}")
     outState?.putString(SELECTED_LANGUAGE, presenter.selectedLanguage)
   }
 
@@ -127,13 +120,13 @@ class MainActivity : AppCompatActivity() {
             )
 
           else -> {
-            Log.d(this.javaClass.simpleName, "Unhandled activity-result result-code: ${resultCode} for request-code ${requestCode}")
+            Timber.d("Unhandled activity result code: ${resultCode} for request-code ${requestCode}")
           }
         }
       }
 
       else -> {
-        Log.d(this.javaClass.simpleName, "Unknown activity-result request-code: ${requestCode}")
+        Timber.d("Unknown activity-result request-code: ${requestCode}")
       }
     }
   }
@@ -144,7 +137,7 @@ class MainActivity : AppCompatActivity() {
         presenter.playInstruction(item as InstructionViewModel)
       }
       else {
-        Log.e(this.javaClass.simpleName, "Cannot prepare to play instruction because ${item}, the ${position + 1}th item in the list of instructions, is not a valid instruction.")
+        presenter.couldNotPlayNonInstruction(item, position)
       }
     }
 
@@ -198,7 +191,6 @@ class MainActivity : AppCompatActivity() {
 
   fun showInstructions() {
     if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-      Log.d(this.javaClass.simpleName, "About to get instructions")
       presenter.showInstructions()
     }
     else {
@@ -212,7 +204,6 @@ class MainActivity : AppCompatActivity() {
       // this thread waiting for the user's response! After the user sees the
       // explanation (dialog or snackbar are easy), try again to request the
       // permission.
-      Log.d("TODO:", "Show request permission rationale for write external storage.")
       ActivityCompat.requestPermissions(
         this,
         arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
@@ -247,8 +238,6 @@ class MainActivity : AppCompatActivity() {
   fun refreshUnparsableInstructions(
     unparsableInstructions: ImmutableList<UnparsableInstructionViewModel>
   ) {
-    Log.d(this.javaClass.simpleName, "refreshUnparsableInstructions")
-    Log.d("unpars instructs", unparsableInstructions.toString())
     if (unparsableInstructions.isEmpty()) {
       binding.unparsableInstructionsContainer.visibility = View.GONE
       (binding.unparsableInstructions.adapter as UnparsableInstructionsAdapter)
@@ -262,27 +251,19 @@ class MainActivity : AppCompatActivity() {
   }
 
   fun selectLanguageTab(index: Int) {
-    Log.d(this.javaClass.simpleName, "selectLanguageTab")
-    Log.d("index", index.toString())
     val tab = binding.languages.getTabAt(index)
     if (tab != null) {
-      Log.d(this.javaClass.simpleName, "programmatically selecting tab for language ${tab.tag}")
       tab.select()
     }
     else {
-      Log.d("selectLanguageTab", "Tab of index ${index} is not available. Selected default language tab instead.")
       binding.languages.getTabAt(0)?.select()
     }
   }
 
   fun refreshLanguageTabs(languages: ImmutableList<String>) {
-    // 2017-01-28 Cort Spellman
-    // TODO: Use a viewpager -- this is stupid to be depending on the tabs
-    // being in the same order as another list.
-    Log.d(this.javaClass.simpleName, "refreshLanguageTabs")
-    Log.d("languages", languages.toString())
+    // 2017-02-25 Cort Spellman
+    // TODO: Can I use a ViewPager for this instead?
     val languageTabs = binding.languages
-    val previouslySelectedLanguage = presenter.selectedLanguage
 
     languageTabs.removeAllTabs()
 
@@ -290,16 +271,9 @@ class MainActivity : AppCompatActivity() {
       languageTabs.addTab(
         languageTabs.newTab().setTag(l).setText(l))
     }
-
-    previouslySelectedLanguage?.let {
-      Log.d("refreshLanguagesTabs", "Setting language to selected language of ${previouslySelectedLanguage}.")
-      selectLanguageTab(languages.indexOf(previouslySelectedLanguage))
-    }
   }
 
   fun refreshInstructionsForCurrentLanguage(instructions: ImmutableList<InstructionViewModel>) {
-    Log.d(this.javaClass.simpleName, "refreshInstructionsForCurrentLanguage")
-    Log.d("instructions", instructions.toString())
     (binding.instructions.adapter as InstructionsAdapter)
       .refreshInstructions(instructions)
   }

app/src/main/kotlin/com/cws/image/MainPresenter.kt
@@ -1,9 +1,13 @@
 package com.cws.image
 
-import android.util.Log
 import com.github.andrewoma.dexx.kollection.*
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.BiFunction
 import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
+import java.io.IOException
+import java.util.concurrent.TimeUnit
 import kotlin.comparisons.compareBy
 import kotlin.comparisons.thenBy
 
@@ -15,18 +19,35 @@ class MainPresenter(
   var selectedLanguage: String? = null
 
   fun showInstructions() {
-    Log.d(this.javaClass.simpleName, "About to get instructions")
     getInstructions.getInstructions()
-      .doOnSuccess { parsedInstructions ->
-        instructions = instructionsToInstructionViewModels(
-          parsedInstructions.instructions,
-          parsedInstructions.icons)
-        Log.d(this.javaClass.simpleName, "Instructions: ${instructions}")
+      .toObservable()
+      .retryWhen { errors ->
+        errors
+          .zipWith(Observable.range(4, 12),
+                   BiFunction({ error: Throwable, i: Int -> i }))
+          .flatMap { numberOfRetries ->
+            // 2017-02-19 Cort Spellman
+            // integral(2 ^ x, x, x = 4, x = 8, x <- Z) = 496
+            // 496 milliseconds ~ a half-second delay
+            if (numberOfRetries == 8) {
+              // TODO: Show snackbar message "Loading..." or similar.
+            }
+
+            // 2017-02-19 Cort Spellman: Retry with exponential backoff.
+            val durationUntilRetry =
+              Math.pow(2.toDouble(), numberOfRetries.toDouble()).toLong()
+
+            Timber.i("Unable to load instructions. Failure #${numberOfRetries}. Trying again in ${durationUntilRetry} milliseconds.")
+            Observable.timer(durationUntilRetry, TimeUnit.MILLISECONDS) }
       }
       .subscribeOn(Schedulers.io())
       .observeOn(AndroidSchedulers.mainThread())
       .subscribe(
         { parsedInstructions ->
+          instructions = instructionsToInstructionViewModels(
+            parsedInstructions.instructions,
+            parsedInstructions.icons)
+
           val unparsableInstructionViewModels =
           parsedInstructions.unparsableFiles
               .map { u: UnparsableFile ->
@@ -47,18 +68,15 @@ class MainPresenter(
           // Switch the unparsable instructions recyclerview to databinding and the
           // languages to a viewpager and databinding so that you can change the
           // viewmodel here instead of manipulating the view manually.
-          Log.d(this.javaClass.simpleName, "About to refresh unparsable instructions")
           activity.refreshUnparsableInstructions(unparsableInstructionViewModels)
-          Log.d(this.javaClass.simpleName, "About to refresh language tabs")
           activity.refreshLanguageTabs(languages)
-          Log.d(this.javaClass.simpleName, "About to set language to ${selectedLanguage}")
           activity.selectLanguageTab(languages.indexOf(selectedLanguage))
         },
         { throwable ->
-          Log.d(this.javaClass.simpleName, "About to show message for failing to read instructions")
-          activity.showMessageForInstructionsLoadFailure(
-            (throwable.message as? String)
-            ?: "Could not read instructions. (No reason given.)")
+          val message = (throwable.message as? String)
+                        ?: "Could not read instructions. (No reason given.)"
+          Timber.e(IOException(message))
+          activity.showMessageForInstructionsLoadFailure(message)
         }
       )
   }
@@ -66,8 +84,8 @@ class MainPresenter(
   fun showInstructionsForLanguage(language: String) {
     // FIXME: Subjects must be mapped to resource strings, to be displayed in
     // the device's current language.
-    // Can I do this in databinding instead?
-    Log.d(this.javaClass.simpleName, "About to set language to ${language}")
+
+    // TODO: Can I do this in databinding instead?
     selectedLanguage = language
     activity.refreshInstructionsForCurrentLanguage(
       instructions.filter { i -> i.language == language }
@@ -80,12 +98,22 @@ class MainPresenter(
     activity.startPlayInstructionActivity(instruction)
   }
 
+  fun couldNotPlayNonInstruction(
+    item: Any?,
+    positionInList: Int
+  ) {
+    Timber.e(
+      Exception(
+        "Cannot play non-instruction item ${item}, element number ${positionInList + 1} in the list of instructions."))
+    activity.showMessageForInstructionPlayFailure("Could not play the selected item.")
+  }
+
   fun couldNotPlayInstruction(instruction: InstructionViewModel?, message: String?) {
     val displayMessage =
       instruction?.let {
         "The ${it.language} ${it.subject} instruction could not be played.\n${message}"
       }
-      ?: "The instruction could not be played."
+      ?: "The instruction could not be played.\n${message}"
     activity.showMessageForInstructionPlayFailure(displayMessage)
   }
 
@@ -93,17 +121,31 @@ class MainPresenter(
     instructions: ImmutableSet<Instruction>,
     icons: ImmutableSet<Icon>
   ): ImmutableSet<InstructionViewModel> {
-    val iconPathsBySubject = icons.map { icon -> Pair(icon.subject, icon) }.toImmutableMap()
+    val iconsBySubject = icons.map { icon -> Pair(icon.subject, icon) }.toImmutableMap()
+
+    logMissingIcons(
+      iconsBySubject,
+      instructions.map { instruction -> instruction.subject}.toImmutableSet())
+
     return instructions.map { instruction ->
       InstructionViewModel(
         subject = titleCase(instruction.subject),
         language = instruction.language,
         audioAbsolutePath = instruction.absolutePath,
         cueStartTimeMilliseconds = instruction.cueStartTime,
-        iconAbsolutePath = iconPathsBySubject[instruction.subject]?.absolutePath
+        iconAbsolutePath = iconsBySubject[instruction.subject]?.absolutePath
       )
     }
       .toImmutableSet()
+  }
+
+  fun logMissingIcons(
+    iconsBySubject: ImmutableMap<String, Icon>,
+    subjects: ImmutableSet<String>
+  ) {
+    subjects.forEach { subject ->
+      iconsBySubject[subject] ?: Timber.w("No icon for subject ${subject}")
+    }
   }
 
   fun sortLanguages(ls : Iterable<String>) : List<String> {

app/build.gradle
@@ -111,7 +111,7 @@ dependencies {
   compile "io.reactivex.rxjava2:rxjava:2.0.6"
   compile "io.reactivex.rxjava2:rxandroid:2.0.1"
 
-  compile "com.github.akarnokd:rxjava2-interop:0.9.1"
+  compile "com.github.akarnokd:rxjava2-interop:0.9.2"
 
   compile "com.jakewharton.rxbinding:rxbinding-kotlin:1.0.0"
   compile "com.jakewharton.rxbinding:rxbinding-appcompat-v7-kotlin:1.0.0"
@@ -119,11 +119,16 @@ dependencies {
   compile "com.jakewharton.rxbinding:rxbinding-recyclerview-v7-kotlin:1.0.0"
   compile "com.jakewharton.rxbinding:rxbinding-support-v4-kotlin:1.0.0"
 
-  compile "com.github.bumptech.glide:glide:3.7.0"
+  compile "com.squareup.picasso:picasso:2.5.2"
+
+  compile "com.jakewharton.timber:timber:4.5.1"
 
   compile("com.crashlytics.sdk.android:crashlytics:2.6.6@aar") {
-        transitive = true;
-    }
+    transitive = true;
+  }
+  compile("com.crashlytics.sdk.android:answers:1.3.11@aar") {
+    transitive = true;
+  }
 
   debugCompile "com.squareup.leakcanary:leakcanary-android:1.5"
 
@@ -131,7 +136,7 @@ dependencies {
 
   compile "com.facebook.stetho:stetho:1.4.2"
   compile "com.facebook.stetho:stetho-okhttp3:1.4.2"
-  compile "com.android.support.constraint:constraint-layout:1.0.0-beta5"
+  compile 'com.android.support.constraint:constraint-layout:1.0.0'
 }
 
 kapt {

app/src/main/kotlin/com/cws/image/App.kt
@@ -3,10 +3,12 @@ package com.cws.image
 import android.app.Application
 import android.os.Environment
 import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.answers.Answers
 import com.facebook.stetho.Stetho
 import com.squareup.leakcanary.LeakCanary
 import com.squareup.leakcanary.RefWatcher
 import io.fabric.sdk.android.Fabric
+import timber.log.Timber
 import java.io.File
 
 class App : Application() {
@@ -30,8 +32,16 @@ class App : Application() {
   // current locale.
 
   override fun onCreate() {
+    // 2017-02-23 Cort Spellman
+    // TODO: Add app-crash handler: slides 13 and 14 at
+    // https://www.slideshare.net/Infinum/infinum-android-talks-15-timber-crashlytics-a-match-made-in-heaven
     super.onCreate()
     Fabric.with(this, Crashlytics())
+    Fabric.with(this, Answers())
+    if (BuildConfig.DEBUG) {
+        Timber.plant(Timber.DebugTree())
+    }
+    Timber.plant(CrashlyticsTree())
     refWatcher = LeakCanary.install(this)
     Stetho.initializeWithDefaults(this)
   }

app/src/main/kotlin/com/cws/image/CrashlyticsTree.kt
@@ -1,0 +1,22 @@
+package com.cws.image
+
+import android.util.Log
+import com.crashlytics.android.Crashlytics
+import timber.log.Timber
+
+class CrashlyticsTree : Timber.Tree() {
+  override fun log(priority: Int, tag: String?, message: String?, t: Throwable?) {
+    if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+      return
+    }
+
+    // 2017-02-23 Does not log to logcat.
+    // "... every time you log in production, a puppy dies."
+    //   -- https://github.com/JakeWharton/timber
+    Crashlytics.log(message)
+
+    if (t != null) {
+      Crashlytics.logException(t)
+    }
+  }
+}

app/src/main/kotlin/com/cws/image/GetInstructions.kt
@@ -1,9 +1,9 @@
 package com.cws.image
 
-import android.util.Log
 import com.github.andrewoma.dexx.kollection.ImmutableSet
 import com.github.andrewoma.dexx.kollection.toImmutableSet
 import io.reactivex.Single
+import timber.log.Timber
 import java.io.File
 import java.io.UnsupportedEncodingException
 import java.net.URLDecoder
@@ -45,17 +45,12 @@ class GetInstructions(
   var parsedResourcess: ParsedResourcess? = null
 
   fun getInstructions(): Single<ParsedResourcess> {
-    Log.d(this.javaClass.simpleName, "About to get instructions")
     if (parsedResourcess != null) {
-      Log.d(this.javaClass.simpleName, "Returning saved parsed-instructions.")
       return Single.just(parsedResourcess)
     }
     else {
-      Log.d(this.javaClass.simpleName, "No parsed-instructions saved. Loading and parsing instructions now.")
       return instructionsGateway.getInstructionFiles()
         .map { instructionFiles ->
-          Log.d(this.javaClass.simpleName,
-                "Loaded instruction files; about to parse instructions")
           val parseResults = instructionFiles.map { file ->
             parse(file)
           }
@@ -63,18 +58,14 @@ class GetInstructions(
           val unparsableFiles = parseResults.filterIsInstance<Result.Err<UnparsableFile, Resource>>()
             .map { x -> x.errValue }
             .toImmutableSet()
-          Log.d(this.javaClass.simpleName, "unparsableFiles: ${unparsableFiles}")
           val resources = parseResults.filterIsInstance<Result.Ok<UnparsableFile, Resource>>()
             .map { x -> x.okValue }
-          Log.d(this.javaClass.simpleName, "resources: ${resources}")
 
           parsedResourcess = ParsedResourcess(
             unparsableFiles = unparsableFiles,
             instructions = resources.filterIsInstance<Instruction>().toImmutableSet(),
             icons = resources.filterIsInstance<Icon>().toImmutableSet()
           )
-          Log.d(this.javaClass.simpleName, "Parsed resources")
-          Log.d(this.javaClass.simpleName, parsedResourcess.toString())
           parsedResourcess
         }
     }
@@ -91,12 +82,17 @@ class GetInstructions(
         fileToIcon(file, fileName)
       }
       else {
+        Timber.e(
+          Exception(
+            "File extension indicates neither an audio file nor an icon file: ${fileName}"))
         return Result.Err(
           UnparsableFile(file.absolutePath, ParsingFailure.FileFormat()))
       }
     }
     catch (e: UnsupportedEncodingException) {
-      Log.e("parse file", "parse failure: ${e}")
+      Timber.e(
+        Exception(
+          "Filename does not consist of valid UTF-8 characters: ${file}", e))
       Result.Err<UnparsableFile, Resource>(
         UnparsableFile(file.name,
                        ParsingFailure.FileNameEncoding()))
@@ -104,12 +100,9 @@ class GetInstructions(
   }
 
   fun fileToInstruction(file: File, fileName: String): Result<UnparsableFile, Instruction> {
-    Log.d("parse instruction", "Instruction file to parse: ${file.absolutePath}")
     return try {
       val (subject, language, cueTime) =
         fileName.substringBeforeLast(".").split('_')
-
-      Log.d("parse instruction", "subject: ${subject}    language: ${language}    cueStartTimeMilliseconds: ${cueTime}")
 
       Result.Ok(
         Instruction(
@@ -119,15 +112,17 @@ class GetInstructions(
           cueStartTime = cueTime.toLong()))
     }
     catch (e: IndexOutOfBoundsException) {
-      e.printStackTrace()
-      Log.e("parse instruction", "parse failure: ${e}")
+      Timber.e(
+        Exception(
+          "Invalid instruction file name format: ${file}"))
       Result.Err(
         UnparsableFile(file.name,
                        ParsingFailure.InstructionFileNameFormat()))
     }
     catch (e: NumberFormatException) {
-      e.printStackTrace()
-      Log.e("parse instruction", "parse failure: ${e}")
+      Timber.e(
+        NumberFormatException(
+          "Invalid instruction cue time in file name: ${file}"))
       Result.Err(
         UnparsableFile(file.name,
                        ParsingFailure.InstructionCueTime()))
@@ -135,10 +130,7 @@ class GetInstructions(
   }
 
   fun fileToIcon(file: File, fileName: String): Result<UnparsableFile, Icon> {
-    Log.d("parse icon", "Icon file to parse: ${file.absolutePath}")
     val subject = fileName.substringBeforeLast(".")
-
-    Log.d("parse icon", "subject: ${subject}")
 
     return Result.Ok(
       Icon(

app/src/main/kotlin/com/cws/image/InstructionsAdapter.kt
@@ -3,15 +3,17 @@ package com.cws.image
 import android.content.Context
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
-import android.graphics.Color
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.bumptech.glide.Glide
 import com.cws.image.databinding.SubjectLayoutBinding
 import com.github.andrewoma.dexx.kollection.ImmutableList
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import timber.log.Timber
+import java.io.File
 
 class InstructionsAdapter(
   val layoutId: Int,
@@ -41,15 +43,25 @@ class InstructionsAdapter(
       (holder?.binding as? SubjectLayoutBinding)?.subjectIcon
 
     if (instructionViewModel.iconAbsolutePath != null) {
-      Glide.with(context)
-        .load(instructionViewModel.iconAbsolutePath)
-        .asBitmap()
-        .placeholder(ContextCompat.getDrawable(context, R.drawable.ic_subject_placeholder))
-        .into(subjectIconImageView)
+      Picasso.with(context)
+        .load(File(instructionViewModel.iconAbsolutePath))
+        .error(R.drawable.ic_subject_placeholder)
+        .into(subjectIconImageView, object: Callback {
+          override fun onSuccess() {}
 
+          override fun onError() {
+            Timber.i("Instruction: ${instructionViewModel}")
+            Timber.e(
+              Exception(
+                "Failed to load subject icon ${instructionViewModel.iconAbsolutePath}"))
+            subjectIconImageView?.setColorFilter(
+              ContextCompat.getColor(context, R.color.placeholderIcon))
+          }
+        })
     }
     else {
-      subjectIconImageView?.setColorFilter(Color.parseColor("#FFC0C0C0"))
+      subjectIconImageView?.setColorFilter(
+        ContextCompat.getColor(context, R.color.placeholderIcon))
       subjectIconImageView?.setImageDrawable(
         ContextCompat.getDrawable(context, R.drawable.ic_subject_placeholder))
     }

app/src/main/kotlin/com/cws/image/InstructionsRepository.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Environment
-import android.util.Log
 import com.github.andrewoma.dexx.kollection.ImmutableSet
 import com.github.andrewoma.dexx.kollection.immutableSetOf
 import com.github.andrewoma.dexx.kollection.toImmutableSet
@@ -22,7 +21,6 @@ class InstructionsRepository(
       context, storageDir, tokenFileToMakeDirAppearWhenDeviceIsMountedViaUsb)
 
   override fun getInstructionFiles(): Single<ImmutableSet<File>> {
-    Log.d(this.javaClass.simpleName, "About to get instruction files")
     return localFileSystemInstructionsStorage.getInstructionFiles()
   }
 }
@@ -47,21 +45,12 @@ class LocalFileSystemInstructionsStorage(
   }
 
   fun getInstructionFiles(): Single<ImmutableSet<File>> {
-    Log.d(this.javaClass.simpleName, "About to get instruction files")
-    return ensureInstructionsDirExists()
-      .doOnSuccess { dir ->
-        Log.d(this.javaClass.simpleName, "Ensured instructions dir exists: ${dir}")
-      }
+    return Single.just(ensureInstructionsDirExists())
       .flatMap { dir -> ensureInstructionsDirIsAccessibleFromPC() }
-      .doOnSuccess { dir ->
-        Log.d(this.javaClass.simpleName, "Ensured instructions dir is accessible from a PC: ${dir}")
-      }
       .map { dir ->
         if (!isExternalStorageReadable()) {
           throw IOException("External storage is not readable.")
         }
-
-        Log.d(this.javaClass.simpleName, "About to emit instruction files")
 
         storageDir.listFiles()
           .filter { file -> !filesToSkip.contains(file) }
@@ -69,8 +58,7 @@ class LocalFileSystemInstructionsStorage(
       }
   }
 
-  fun ensureInstructionsDirExists(): Single<File> {
-    Log.d(this.javaClass.simpleName, "About to ensure instructions dir exists")
+  fun ensureInstructionsDirExists(): File {
     return if (!isExternalStorageReadable()) {
       throw IOException("External storage is not readable.")
     }
@@ -84,19 +72,21 @@ class LocalFileSystemInstructionsStorage(
             throw IOException("Could not create directory ${storageDir.absolutePath}, even though external storage is writable.")
           }
           else {
-            Single.just(storageDir)
+            storageDir
           }
         }
       }
       else {
-        Single.just(storageDir)
+        storageDir
       }
     }
   }
 
   fun ensureInstructionsDirIsAccessibleFromPC(): Single<File> {
-    Log.d(this.javaClass.simpleName, "About to ensure instructions dir is accessible from a PC")
-    return if (!tokenFileToMakeDirAppearWhenDeviceIsMountedViaUsb.isFile) {
+    return if (tokenFileToMakeDirAppearWhenDeviceIsMountedViaUsb.isFile) {
+      Single.just(storageDir)
+    }
+    else {
       if (!isExternalStorageWritable()) {
         throw IOException(
           "There is no token file in the instructions directory, ${storageDir.absolutePath} and it can't be created because external storage is not writable.")
@@ -107,8 +97,6 @@ class LocalFileSystemInstructionsStorage(
             "Could not create file ${tokenFileToMakeDirAppearWhenDeviceIsMountedViaUsb.absolutePath}, even though external storage is writable.")
         }
         else {
-          Log.d("instructions dir",
-                "Created file ${tokenFileToMakeDirAppearWhenDeviceIsMountedViaUsb.absolutePath} (to make directory appear when device is mounted via USB).")
           Single.create<File>(
             { emitter ->
               MediaScannerConnection.scanFile(
@@ -118,8 +106,6 @@ class LocalFileSystemInstructionsStorage(
                 null,
                 { path, uri ->
                   if (uri is Uri) {
-                    Log.d("instructions dir", "Scanned ${path}:")
-                    Log.d("instructions dir", "-> uri=${uri}")
                     emitter.onSuccess(storageDir)
                   }
                   else {
@@ -131,9 +117,6 @@ class LocalFileSystemInstructionsStorage(
             })
         }
       }
-    }
-    else {
-      Single.just(storageDir)
     }
   }
 }

commit 660d0a1d5060003549b04b05f92c83d581200f90

Fix: Maintain selected language on config change.
I introduced this regression in
4ceb8782d781f10f1aceca05eb07a1b2edaa8895. I added a comment to
MainActivity#refreshLanguageTabs to prevent the same mistake in the
future.

TODO: ADD TESTS! We're beyond the prototype stage.

app/src/main/kotlin/com/cws/image/MainActivity.kt
@@ -87,7 +87,9 @@ class MainActivity : AppCompatActivity() {
         }
 
     if (savedInstanceState != null) {
-      presenter.selectedLanguage = savedInstanceState.getString(SELECTED_LANGUAGE)
+      val savedSelectedLanguage = savedInstanceState.getString(SELECTED_LANGUAGE)
+      Timber.d("Restoring language selection: ${savedSelectedLanguage}")
+      presenter.selectedLanguage = savedSelectedLanguage
     }
 
     showInstructions()
@@ -95,6 +97,7 @@ class MainActivity : AppCompatActivity() {
 
   override fun onSaveInstanceState(outState: Bundle?) {
     super.onSaveInstanceState(outState)
+    Timber.d("Saving language selection: ${presenter.selectedLanguage}")
     outState?.putString(SELECTED_LANGUAGE, presenter.selectedLanguage)
   }
 
@@ -261,8 +264,7 @@ class MainActivity : AppCompatActivity() {
   }
 
   fun refreshLanguageTabs(languages: ImmutableList<String>) {
-    // 2017-02-25 Cort Spellman
-    // TODO: Can I use a ViewPager for this instead?
+    val previouslySelectedLanguage = presenter.selectedLanguage
     val languageTabs = binding.languages
 
     languageTabs.removeAllTabs()
@@ -270,6 +272,17 @@ class MainActivity : AppCompatActivity() {
     languages.forEach { l ->
       languageTabs.addTab(
         languageTabs.newTab().setTag(l).setText(l))
+    }
+
+    // 2017-02-25 Cort Spellman
+    // TODO: Can I use a ViewPager instead of adding tabs manually above?
+    // When there are no tabs, the first tab added becomes the selected tab,
+    // necessitating this reset to the previously selected language.
+    // NOTE: I have to reset here because selecting the tab, whether by a
+    // user's click or by TabLayout#addTab when there are no tabs, sets
+    // MainPresenter#selectedLanguage.
+    previouslySelectedLanguage?.let {
+      selectLanguageTab(languages.indexOf(previouslySelectedLanguage))
     }
   }
 

app/src/main/kotlin/com/cws/image/MainPresenter.kt
@@ -70,7 +70,6 @@ class MainPresenter(
           // viewmodel here instead of manipulating the view manually.
           activity.refreshUnparsableInstructions(unparsableInstructionViewModels)
           activity.refreshLanguageTabs(languages)
-          activity.selectLanguageTab(languages.indexOf(selectedLanguage))
         },
         { throwable ->
           val message = (throwable.message as? String)