5e4bd7065e28fbc3933efdf9843a5a48ff8791f5
Cort Spellman <spellman.cort@gmail.com>
2017-03-04 16:12:21 -0600

    Update dependencies.

--- app/src/main/kotlin/com/cws/image/BaseFragment.kt
+++ app/src/main/kotlin/com/cws/image/BaseFragment.kt
@@ -2,7 +2,7 @@ package com.cws.image
 
 import android.support.v4.app.Fragment
 
-open class BaseFragment() : Fragment() {
+open class BaseFragment : Fragment() {
   override fun onDestroy() {
     super.onDestroy()
     (activity.application as App).refWatcher.watch(this)
